d9156cfcd13b528c90dab533e055762403c38640
Michał Rudzki <michal@mailx.com.pl>
2023-01-21 15:44:30 +0100

    main program file, I add structure and menu

--- magazyn.py
+++ magazyn.py
@@ -1,7 +1,16 @@
 import sys
+from modules_warehouse import general as ci
+
+MENU_LIST = ['saldo', 'sprzedaz', 'zakup', 'konto', 'lista', 'magazyn', 'przegld', 'koniec']
 
 def main():
-    pass
+    while True:
+        ci.show_menu(MENU_LIST)
+        user_input = input("->")
+        if user_input != "koniec":
+            print("menu list")
+        else:
+            break
 
 if __name__ == "__main__":
     main()
