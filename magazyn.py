7f5ebdeea7618fbca2f47c740340f5a254465c50
Michał Rudzki <michal@mailx.com.pl>
2023-01-24 17:14:25 +0100

    another approuch

--- magazyn.py
+++ magazyn.py
@@ -1,15 +1,8 @@
 import sys
-from modules_warehouse import general as ci
-
-MENU_LIST = ['saldo', 'sprzedaz', 'zakup', 'konto', 'lista', 'magazyn', 'przegld', 'koniec']
 
 def main():
     while True:
-        ci.show_menu(MENU_LIST)
-        user_input = input("->")
-        if user_input != "koniec":
-            print("menu list")
-        else:
+        if not True:
             break
 
 if __name__ == "__main__":
