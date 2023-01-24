9f6bd5dd99b62a73704848e1afa477a3121af87e
Michał Rudzki <michal@mailx.com.pl>
2023-01-24 21:17:34 +0100

    it works with saldo and zakup, works with the in.txt from 1 and 2 from kurs_python_zadanie_3_3

--- magazyn.py
+++ magazyn.py
@@ -1,9 +1,24 @@
 import sys
 
 def main():
+    data = []
     while True:
-        if not True:
+        user_input = input()
+        if not user_input or user_input == 'stop':
             break
+        data.append(user_input)
+
+    for arg in range(len(sys.argv)-1):
+        data.append(sys.argv[arg+1])
+    for data_count in data:
+        print(data_count)
+    
+    #if sys.argv[1] == 'zakup':
+    #    for arg in range(len(sys.argv)-1):
+    #        data.append(sys.argv[arg+1])
+    #    
+    #    for data_count in data:
+    #        print(data_count)
 
 if __name__ == "__main__":
     main()
