5b771036ff295ee3d221dc05e631c0e0a9a31f3c
Michał Rudzki <michal@mailx.com.pl>
2023-01-27 20:35:03 +0100

    some diffrent trys.., no luck

--- magazyn.py
+++ magazyn.py
@@ -13,12 +13,20 @@ def main():
     for data_count in data:
         print(data_count)
     
-    #if sys.argv[1] == 'zakup':
-    #    for arg in range(len(sys.argv)-1):
-    #        data.append(sys.argv[arg+1])
-    #    
-    #    for data_count in data:
-    #        print(data_count)
-
+    # warehouse products
+    warehouse = {}
+    
+        
+    #for count in range(len(data)):
+    #    if data[count] == 'zakup':
+    #        warehouse.update({data[count+1]:int(data[count+3])})
+    #    elif data[count] == 'sprzedaż':
+    #        if data[count+1] in warehouse.keys():
+    #            #warehouse[data[count+1]] = warehouse[data[count+1]] - data[count+3]
+    #            warehouse[data[count+1]] = warehouse[data[count+1]] - int(data[count+3])
+            
+    
+    print(warehouse)
+    
 if __name__ == "__main__":
     main()
