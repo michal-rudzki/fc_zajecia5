d02fb1374cd1e8c25bca960fc39b48bbecd27d6e
Michał Rudzki <michal@mailx.com.pl>
2023-02-26 19:01:09 +0100

    db file for magazyn.py

---
+++ README.md
@@ -0,0 +1,46 @@
+Napisz program, który będzie rejestrował operacje na koncie firmy i stan magazynu.
+
+Program po uruchomieniu wyświetla informację o dostępnych komendach:
+
+    saldo
+    sprzedaż
+    zakup
+    konto
+    lista
+    magazyn
+    przegląd
+    koniec
+
+Po wprowadzeniu odpowiedniej komendy, aplikacja zachowuje się w unikalny sposób dla każdej z nich:
+
+    saldo - Program pobiera kwotę do dodania lub odjęcia z konta.
+    sprzedaż - Program pobiera nazwę produktu, cenę oraz liczbę sztuk. Produkt musi znajdować się w magazynie. Obliczenia respektuje względem konta i magazynu (np. produkt "rower" o cenie 100 i jednej sztuce spowoduje odjęcie z magazynu produktu "rower" oraz dodanie do konta kwoty 100).
+    zakup - Program pobiera nazwę produktu, cenę oraz liczbę sztuk. Produkt zostaje dodany do magazynu, jeśli go nie było. Obliczenia są wykonane odwrotnie do komendy "sprzedaz". Saldo konta po zakończeniu operacji „zakup” nie może być ujemne.
+    konto - Program wyświetla stan konta.
+    lista - Program wyświetla całkowity stan magazynu wraz z cenami produktów i ich ilością.
+    magazyn - Program wyświetla stan magazynu dla konkretnego produktu. Należy podać jego nazwę.
+    przegląd - Program pobiera dwie zmienne „od” i „do”, na ich podstawie wyświetla wszystkie wprowadzone akcje zapisane pod indeksami od „od” do „do”. Jeżeli użytkownik podał pustą wartość „od” lub „do”, program powinien wypisać przegląd od początku lub/i do końca. Jeżeli użytkownik podał zmienne spoza zakresu, program powinien o tym poinformować i wyświetlić liczbę zapisanych komend (żeby pozwolić użytkownikowi wybrać odpowiedni zakres).
+    koniec - Aplikacja kończy działanie.
+
+Dodatkowe wymagania:
+
+    Aplikacja od uruchomienia działa tak długo, aż podamy komendę "koniec".
+    Komendy saldo, sprzedaż i zakup są zapamiętywane przez program, aby móc użyć komendy "przeglad".
+    Po wykonaniu dowolnej komendy (np. "saldo") aplikacja ponownie wyświetla informację o dostępnych komendach, a także prosi o wprowadzenie jednej z nich.
+    Zadbaj o błędy, które mogą się pojawić w trakcie wykonywania operacji (np. przy komendzie "zakup" jeśli dla produktu podamy ujemną kwotę, aplikacja powinna wyświetlić informację o niemożności wykonania operacji i jej nie wykonać). Zadbaj też o prawidłowe typy danych.
+
+Używanie programu:
+
+```
+$ python magazyn.py opcje parametry < in.txt
+```
+
+Przypadki użycia w pliku dla vscode: *launch.json*
+
+jak poniżej zostało przetestowane:
+- :white_check_mark: "args": ["saldo", "-1000", "podatek", "<", "in.txt"]
+- :white_check_mark: "args": ["zakup", "jetson", "40000", "5", "<", "in.txt"]
+- :white_check_mark: "args": ["sprzedaż", "jetson", "50000", "4", "<", "in.txt"]
+- :white_check_mark: "args": ["magazyn", "jetson", "raspberry", "arduino", "satel", "<", "in.txt"]
+- :white_check_mark: "args": ["konto", "<", "in.txt"]
+- :white_check_mark: "args": ["przegląd", "0", "1", "<", "in.txt"]
---
+++ magazyn.py
@@ -0,0 +1,134 @@
+import sys
+
+def check_warehouse(warehouse, data):
+    for product in warehouse:
+        if product == data:
+            return True
+    return False
+
+def main():
+    data = []
+    konto = []
+    data_review = []
+    
+    warehouse = {
+        'saldo': {},
+        'sprzedaż': {},
+        'zakup': {},
+        'magazyn': {}
+    }
+
+    while True:
+        user_input = input().strip()
+        if not user_input or user_input == 'stop':
+            break
+        data.append(user_input)
+    
+    data_review = data
+        
+    for arg in sys.argv[1:]:
+        data.append(arg)
+        
+    
+    for loop in range(len(data)):
+        if data[0] == 'saldo':
+            if not warehouse[data[0]]:
+                warehouse.update({len(warehouse[data[0]]):{data[0]:{int(data[1]):data[2]}}})
+            warehouse[data[0]].update({len(warehouse[data[0]]):{int(data[1]):data[2]}})
+            data = data[3:]
+            if len(data) == 0:
+                break
+        elif data[0] == 'sprzedaż':
+            if not warehouse[data[0]]:
+                warehouse.update({data[0]:{data[1]:{data[2]:int(data[3])}}})
+                warehouse['saldo'].update({len(warehouse['saldo']):{int(data[2]):data[1]}})
+            warehouse[data[0]].update({data[1]:{data[2]:int(data[3])}})
+            if warehouse['magazyn'][data[1]] - warehouse[data[0]][data[1]][data[2]] >= 0:
+                warehouse['magazyn'][data[1]] = warehouse['magazyn'][data[1]] - warehouse[data[0]][data[1]][data[2]]
+                warehouse['saldo'].update({len(warehouse['saldo']):{int(data[2]):data[1]}})
+            else:
+                print("za mało w magazynie")
+            data = data[4:]
+            if len(data) == 0:
+                break
+        elif data[0] == 'zakup':
+            if not warehouse[data[0]]:
+                warehouse.update({data[0]:{data[1]:{data[2]:int(data[3])}}})
+                warehouse['magazyn'].update({data[1]:int(data[3])})
+                warehouse['saldo'].update({len(warehouse['saldo']):{(int(data[2])*-1):data[1]}})
+                data = data[4:]
+                continue
+            # jesli jest to updejct ilosci, jesli nie ma to wpisz
+            if check_warehouse(warehouse['magazyn'], data[1]) == True:
+                warehouse['magazyn'][data[1]] = warehouse['magazyn'][data[1]] + int(data[3])
+                warehouse['saldo'].update({len(warehouse['saldo']):{(int(data[2])*-1):data[1]}})
+                data = data[4:]
+                if len(data) == 0:
+                    break
+            else:
+                warehouse[data[0]].update({data[1]:{data[2]:int(data[3])}})
+                warehouse['magazyn'].update({data[1]:int(data[3])})
+                warehouse['saldo'].update({len(warehouse['saldo']):{(int(data[2])*-1):data[1]}})
+                data = data[4:]
+                if len(data) == 0:
+                    break
+        # dodac do magazynu z cli
+        elif data[0] == 'magazyn':
+            for product in data[1:]:
+                if check_warehouse(warehouse['magazyn'], product) != True:
+                    warehouse['magazyn'].update({product:0})
+                    if len(data[4:]) == 0:
+                        data = ['stop']
+                        break
+                
+
+    if sys.argv[1] == 'magazyn':
+        print('M A G A Z Y N')
+        for key, value in warehouse['magazyn'].items():
+            print(f"{key}: [{value}]")
+    elif sys.argv[1] == 'konto':
+        for key, value in warehouse['saldo'].items():
+            konto += value.keys()
+        print(f"Stan konta: {sum(konto)}")
+    elif sys.argv[1] == 'przegląd':
+        history = {}
+        index = 0
+        for review in data_review:
+            if review == 'saldo':
+                history.update({index:data_review[0:3]})
+                data_review = data_review[3:]
+                #print(history[index])
+                index += 1
+            elif review == 'zakup' or review == 'sprzedaż':
+                history.update(({index:data_review[0:4]}))
+                data_review = data_review[4:]
+                #print(history[index])
+                index += 1
+        if int(sys.argv[3])+1 <= index:
+            for review in range(int(sys.argv[2]), int(sys.argv[3])+1):
+                for format_history in history[review]:
+                    print(format_history)
+        else:
+            print(f"Index poza zasiegiem, index = [{index-1}]")
+    elif sys.argv[1] == 'saldo':
+        for data in data_review:
+            print(data)
+    elif sys.argv[1] == 'zakup':
+        saldo = []
+        zakup = sys.argv[3]
+        
+        for key, values in warehouse['saldo'].items():
+            for saldo_list in values.keys():
+                saldo.append(saldo_list)
+        
+        if sum(saldo) >= int(zakup):
+            for data in data_review:
+                print(data)
+        else:
+            print("Za mało środków")
+    elif sys.argv[1] == 'sprzedaż':
+        for data in data_review:
+            print(data)
+        
+if __name__ == "__main__":
+    main()
